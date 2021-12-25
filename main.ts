08ff8998854024bdc0e169d3be16283378590e51
Makurado Hizato <51111242+tak3488@users.noreply.github.com>
2021-12-26 05:07:16 +0900

    first commit

---
+++ main.ts
@@ -0,0 +1,31 @@
+import {app, BrowserWindow} from "electron"
+import path from "path"
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
